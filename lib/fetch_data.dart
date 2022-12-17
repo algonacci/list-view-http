2f266ba4f37421d13bd40a29c23782f53dbd3726
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-17 18:15:43 +0700

    Initial Commit

---
+++ lib/fetch_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FetchData extends StatelessWidget {
+  const FetchData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
